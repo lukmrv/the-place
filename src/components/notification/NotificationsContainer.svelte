b0d53a0a21399b8cb8217f23b9194d596458edd4
Łukasz Sawoń <lukash_savon@mail.ru>
2024-11-15 11:46:56 +0100

    Add max number of notifications@

--- src/components/notification/NotificationsContainer.svelte
+++ src/components/notification/NotificationsContainer.svelte
@@ -2,10 +2,9 @@
 	import { notifications } from './notificationStore';
 	import Notification from './Notification.svelte';
 
-	export let maxNotifications = 3; // Default max number of notifications
-
+	let maxNotifications = 3; // Default max number of notifications
 	// Derived store to limit the number of notifications
-	$: visibleNotifications = $notifications.slice(-maxNotifications);
+	let visibleNotifications = $derived($notifications.slice(-maxNotifications));
 </script>
 
 <div class="fixed bottom-4 left-4 z-50 flex flex-col gap-2">
